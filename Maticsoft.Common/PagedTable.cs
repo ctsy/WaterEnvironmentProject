93158856d156896913ac766c6be59dceb78b98ed
jlw <2393824553@qq.com>
2017-10-28 08:31:42 +0800

    jlw

---
+++ Maticsoft.Common/PagedTable.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+
+namespace Maticsoft.Common
+{
+    public static class PagedTable
+    {
+        /// <summary>
+        /// DataTable分页
+        /// </summary>
+        /// <param name="dt">DataTable</param>
+        /// <param name="PageIndex">页索引,注意：从1开始</param>
+        /// <param name="PageSize">每页大小</param>
+        /// <returns></returns>
+       public static DataTable GetPagedTable(DataTable dt, int PageIndex, int PageSize)
+        {
+            if (PageIndex == 0)
+                return dt;
+            DataTable newdt = dt.Copy();
+            newdt.Clear();
+
+            int rowbegin = (PageIndex - 1) * PageSize;
+            int rowend = PageIndex * PageSize;
+
+            if (rowbegin >= dt.Rows.Count)
+                return newdt;
+
+            if (rowend > dt.Rows.Count)
+                rowend = dt.Rows.Count;
+            for (int i = rowbegin; i <= rowend - 1; i++)
+            {
+                DataRow newdr = newdt.NewRow();
+                DataRow dr = dt.Rows[i];
+                foreach (DataColumn column in dt.Columns)
+                {
+                    newdr[column.ColumnName] = dr[column.ColumnName];
+                }
+                newdt.Rows.Add(newdr);
+            }
+
+            return newdt;
+        }
+    }
+}
