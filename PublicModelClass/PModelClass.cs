da3b8c85d52b29b06902d4f6eb2d3dfe746ef556
jlw <2393824553@qq.com>
2017-10-28 08:31:56 +0800

    jlw

---
+++ PublicModelClass/PModelClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicModelClass
+{ 
+    public  class PModelClass
+    {
+       public static string ConnectionBaseSystem = "";        //基础数据库链接
+       public static string ConnectionCenterReceiveData = ""; //存储数据数据库链接
+       public static string ConnectionPlatFormData = ""; //存储数据数据库链接
+
+    }
+}
