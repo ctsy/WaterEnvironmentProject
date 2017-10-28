33a852257b1109b521bffd66618934a9226e6fab
jlw <2393824553@qq.com>
2017-10-28 08:31:35 +0800

    jlw

---
+++ JsonClass/Json.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonClass
+{
+    public class Json_Login         //登陆
+    {
+
+        public string Name { get; set; }
+        public Boolean Success { get; set; }
+
+    }
+}
